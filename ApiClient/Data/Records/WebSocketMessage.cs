cf3db40e3b757789e02930df9f830c116af7c6e3
ParadiseFallen <yjokerch@gmail.com>
2021-12-01 17:08:35 +0200

    change namespace for WSMessage

---
+++ ApiClient/Data/Records/WebSocketMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.WebSockets;
+
+namespace ApiClient.Data.Records
+{
+    public record WebSocketMessage
+    {
+        public byte[]? Data { get; init; }
+        public WebSocketMessageType Type { get; init; }
+
+        public WebSocketMessage() { }
+        public WebSocketMessage(byte[]? data = null, WebSocketMessageType type = WebSocketMessageType.Binary)
+        {
+            Data = data;
+            Type = type;
+        }
+    }
+}
--- ApiClient/WebSocket/WebSocketClient.cs
+++ ApiClient/WebSocket/WebSocketClient.cs
@@ -8,7 +8,7 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
-using ApiClient.WebSocket.Events;
+using ApiClient.Data.Records;
 
 namespace ApiClient.WebSocket
 {
